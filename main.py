6c995947a6c2f4b9c575493fd5c3ad1fbefbbc4e
Ganesh K <ganeshk.work@gmail.com>
2024-08-17 02:11:49 +0000

    Install bot requirements

--- main.py
+++ main.py
@@ -16,6 +16,21 @@ def bot_get_me():
     tg = Telegram(api_id=api_id, api_hash=api_hash, database_encryption_key=database_encryption_key, phone=phone)
     tg.login()
     tg.add_message_handler(incoming_message_handler) 
+    result = tg.get_chats()
+    print(result.update)
+    result.wait()
+    chat_id1 = -1001740088159
+    # send_message = tg.send_message(chat_id=chat_id1, text="Hello")
+    # send_message.wait()
+    chat_history = tg.get_chat_history(chat_id=-chat_id1)
+    chat_history.wait()
+    print(chat_history.update) 
+    # for chat_id in result.update['chat_ids']:
+    #     print(chat_id)
+    #     chat_history = tg.get_chat_history(chat_id=chat_id)
+    #     chat_history.wait()
+    #     print(chat_history.update)
+        
     tg.idle()
  
 if __name__ == "__main__":
